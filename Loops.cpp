ea17f10ccdea2b488265b27258d18449838a5e85
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2022-02-11 13:02:53 -0700

    Create Loops.cpp

---
+++ Loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+	int i = 60;
+
+	while (i >= -15) {
+		i -= 5;
+		cout << i << endl;
+	}
+	
+	for (int s = 60; s >= -15; s -= 5)
+		cout << s << endl;
+
+
+
+}
